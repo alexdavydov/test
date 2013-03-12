39a284f9fb90a6a99657fa218574905b73d0edb4
Alex Davydov <olexiy.davydov@gmail.com>
2013-03-12 16:31:31 +0200

    Sanitized the conditionals

--- gitbackup.sh
+++ gitbackup.sh
@@ -5,8 +5,8 @@ repo=~/test
 
 while getopts ":m:r:" opt; do #Start the options handling block. We accept -m for commit msg and repo name. Repo dir is assumed to be under ~
 	case $opt in 
-		m ) if [ -n "$OPTARG" ]; then message="$OPTARG"; fi ;; 
-		r ) if [ -n "$OPTARG" ]; then repo="$OPTARG"; fi ;;
+		m ) if [[ -n "$OPTARG" ]]; then message="$OPTARG"; fi ;; 
+		r ) if [[ -n "$OPTARG" ]]; then repo="$OPTARG"; fi ;;
 		\? ) echo $usage 
 		exit 1 ;;
 	esac
@@ -14,12 +14,12 @@ done
 
 shift $((OPTIND - 1))
 
-if [ -z "$*" ]; then	#Exit if no arguments
+if [[ -z "$*" ]]; then	#Exit if no arguments
 	echo $usage
 	exit 1
 fi
 
-if !  [ -d "$repo/.git" ]; then		#Check if the repository exists at given location
+if !  [[ -d "$repo/.git" ]]; then		#Check if the repository exists at given location
 	echo "$repo is not a git repository"
 	exit 1
 #Move the files into repo
@@ -27,9 +27,9 @@ fi
 for filename in "$@"; do 
 	target=$repo/${filename##*/} #Strip directory part and append repo path
 	
-	if [ -e $1 ] && [ -r $1 ];  then 
+	if [[ -e $1 ]] && [[ -r $1 ]];  then 
 
-		if ! [ -e $target ] || [ $filename -nt $target ]; then  #Check if the file already exists in repository, if not, put it there
+		if ! [[ -e $target ]] || [[ $filename -nt $target ]]; then  #Check if the file already exists in repository, if not, put it there
 			cp -r $filename $target
 		fi
 
@@ -41,7 +41,7 @@ done
 
 #Commit and push
 cd $repo
-if [ -n "$message" ]; then 
+if [[ -n "$message" ]]; then 
 	message="-m $message"
 fi
 git commit "$message" #Supply the commit message if present, otherwise we rely on git's prompt
