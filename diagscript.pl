0ca297a3c48f5cc7dd1a7c446d01cc6790fd8a5c
Alex Davydov <olexiy.davydov@gmail.com>
2013-04-02 15:14:21 +0300

    Simple log-parsing script

--- diagscript.pl
+++ diagscript.pl
@@ -5,7 +5,9 @@
 use v5.8.8;
 use strict;
 use warnings;
-#use diagnostics;
+use diagnostics;
+#
+use List::Util qw {reduce};
 
 my %alarm = ();
 
@@ -17,19 +19,35 @@ while (<>) {
 		$alarm {$line[6]}{last_event} = join (" ", @line[0,1,2]) ;  
 		$alarm {$line[6]}{count} += 1;
 	}
-
-#my $time = join (" ",@line[0,1,2]);
-	
-#	my $hostname = @line[3];
-#	my $severity = @line[5];
-#	my $alarm = @line[6];
-#	my $status = @line [7];
-
-
-
-#	print join(" ", @line), "\n";
 }
 
+sub longest {
+	my $max = -1;
+	my $max_ref;
+	for (@_) {
+		if (length > $max) {  # no temp variable, length() twice is faster
+		$max = length;
+		$max_ref = \$_;   # avoid any copying
+    		}
+	}
+	return length ($$max_ref);
+}
+
+my @status = qw (Status);
+my @event = qw ('Last timestamp');
+my @count = qw (Count);
 foreach my $alarmname (keys %alarm) {
-		print $alarmname, " ", $alarm {$alarmname}->{status}, " ", $alarm {$alarmname}->{last_event}, " ", $alarm{$alarmname}{count}, "\n";
+	push @status, $alarm {$alarmname}->{status};
+	push @event, $alarm {$alarmname}->{last_event};
+	push @count, $alarm {$alarmname}->{count};
+}
+
+my $alarmw = longest ("Alarm", keys %alarm) + 1;
+my $statusw = longest (@status) + 1;
+my $eventw = longest (@event) + 1;
+my $countw = longest (@count) + 1;
+
+printf "%-${alarmw}s %-${statusw}s %-${eventw}s %-${countw}s\n", "Alarm", "Status", "Last timestamp", "Count";
+foreach  my $alarmname (keys %alarm) {
+	printf "%-${alarmw}s %-${statusw}s %-${eventw}s %-${countw}s\n", $alarmname, $alarm {$alarmname}->{status}, $alarm {$alarmname}->{last_event}, $alarm{$alarmname}->{count};
 }
